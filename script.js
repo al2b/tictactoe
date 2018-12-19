335de97f73c92e670e3bdb2fee57d0f071001ec3
Anne-Laure de Boissieu <al.deboissieu@elao.com>
2018-12-19 11:39:49 +0100

    fix few troubles

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const player1 = 'player1';
 const player2 = 'player2';
+const buttonReset = 'buttonReset';
 let nextPlayer = null;
 
 let round = 0;
 
-const listNode = document.getElementById('tictac');
+const tableNode = document.getElementById('game');
+const containerNode = document.getElementById('container');
 
-listNode.addEventListener('click', function(event) {
+console.log(containerNode);
+
+containerNode.addEventListener('click', function(event) {
+
+  item = document.getElementById(event.target.id);
+  console.log(item);
+
+  if (item.classList.contains(buttonReset)) {
+    reset();
+  } else {
+    event.stopPropagation();
+  }
+});
+
+tableNode.addEventListener('click', function(event) {
   id = document.getElementById(event.target.id);
+
   if (id.classList.contains(player1) || id.classList.contains(player2)) {
     alert('déjà joué!');
   } else {
@@ -77,4 +94,22 @@ function setNextPlayer() {
   } else {
     nextPlayer = player1
   }
+}
+
+function reset() {
+  const elementsByPlayer1 = document.querySelectorAll(`.${player1} `);
+  const elementsByPlayer2 = document.querySelectorAll(`.${player2} `);
+
+  console.log(elementsByPlayer1);
+
+  elementsByPlayer1.forEach((el) => {
+    el.classList.remove(player1);
+    }
+  );
+  elementsByPlayer2.forEach((el) => {
+    el.classList.remove(player2);
+    }
+  );
+
+  round = 0;
 }
